54e8a2193c2c405e2490e958bd2e299544429e7e
Alex Cruceru <cruceru_alex@hotmail.it>
2026-09-15 13:48:23 +0200

    Complete Adzuna and Freehire extraction

--- src/extract.py
+++ src/extract.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Adzuna extraction
-
 # Define API parameters
 adzuna_app_id = os.getenv("ADZUNA_APP_ID")
 adzuna_app_key = os.getenv("ADZUNA_APP_KEY")
@@ -16,56 +15,146 @@ params = {
     "app_id": adzuna_app_id,
     "app_key": adzuna_app_key,
     "results_per_page": 200,
-    "what": "Data Engineer",
+    "category": "it-jobs",
 }
 
-adzuna_jobs = [] # Stores all jobs extracted from the API
 page = 1
+adzuna_jobs = []
+max_retries = 3
+seen_ids = set()
+
 while True:
     url = f"https://api.adzuna.com/v1/api/jobs/it/search/{page}"
-    response = requests.get(url, params=params)
+
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(
+                url,
+                params=params,
+                timeout=10
+            )
+            break
+        except requests.exceptions.Timeout:
+            print(
+                f"Timeout on page {page} - "
+                f"Attempt {attempt + 1}/{max_retries}"
+            )
+    else:
+        print(
+            f"Failed to retrieve page {page}. "
+            f"Stopping extraction."
+        )
+        break
+
     if response.status_code != 200:
+        print(
+            f"Request failed on page {page}: "
+            f"HTTP {response.status_code}"
+        )
         break
+
     adzuna_data = response.json()
-    if len(adzuna_data["results"]) == 0:
+    results = adzuna_data["results"]
+
+    if len(results) == 0:
+        print(f"No results returned on page {page}.")
         break
-    print(f"Page {page}: {len(adzuna_data['results'])} jobs")
-    adzuna_jobs.extend(adzuna_data["results"]) # Add the jobs from the current page to the list
-    page+=1
 
-print("Adzuna data obtained!")
-with open("data/raw/adzuna_jobs.json", "w") as file:
-    json.dump(adzuna_jobs, file)
-print("Adzuna JSON file created")
+    new_ids = 0
 
-# Jooble extraction
+    for job in results:
+        job_id = job["id"]
 
+        if job_id not in seen_ids:
+            seen_ids.add(job_id)
+            new_ids += 1
+
+    print(
+        f"Page {page}: {len(results)} jobs - "
+        f"{new_ids} new IDs"
+    )
+
+    if new_ids == 0:
+        print("No new job IDs found. Stopping extraction.")
+        break
+
+    adzuna_jobs.extend(results)
+    page += 1
+
+print("Jobs downloaded:", len(adzuna_jobs))
+print("Unique job IDs:", len(seen_ids))
+print(
+    "Total available jobs reported by Adzuna:",
+    adzuna_data["count"]
+)
+with open(
+    "data/raw/adzuna_jobs.json",
+    "w",
+    encoding="utf-8"
+) as file:
+    json.dump(
+        adzuna_jobs,
+        file,
+        ensure_ascii=False,
+        indent=4
+    )
+
+print("Adzuna JSON file created.")
+
+
+# Freehire Extraction
 # Define API parameters
-jooble_app_key = os.getenv("JOOBLE_API_KEY")
-jooble_url = f"https://jooble.org/api/{jooble_app_key}"
-
-headers = {"Content-type": "application/json"}
-body = {
-    "keywords": "IT",
-    "location": "Italy",
-    "page": 1
+url = "https://freehire.me/api/v1/jobs/search"
+params = {
+    "countries": "IT",
+    "is_tech": "tech",
+    "limit": 100,
+    "offset": 0
 }
 
-jooble_jobs = [] # Stores all jobs extracted from the API
-
-while True:
-    response = requests.post(
-        jooble_url,
-        headers=headers,
-        json=body
-    )
-    jooble_data = response.json()
-    if len(jooble_data["jobs"]) == 0: # Stop pagination when the API returns no jobs
+freehire_jobs=[]
+max_retries = 3
+while params["offset"] < 10000:
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(
+                url,
+                params=params,
+                timeout=10
+            )
+            break
+        except requests.exceptions.Timeout:
+                    print(
+                        f"Timeout on offset {params["offset"]} - "
+                        f"Attempt {attempt + 1}/{max_retries}"
+                    )
+    else:
+        print(
+        f"Failed to retrieve offset: {params["offset"]}. "
+        f"Stopping extraction."
+        )
         break
-    jooble_jobs.extend(jooble_data["jobs"]) 
-    body["page"]+= 1
+    if response.status_code != 200:
+        print(
+            f"Request failed on offset {params['offset']}: "
+            f"HTTP {response.status_code}"
+        )
+        break
+    freehire_data = response.json()
+    print(f"Offset: {params['offset']}, Jobs: {len(freehire_data['data'])}")
+    if len(freehire_data["data"]) == 0: # Stop pagination when the API returns no jobs
+        break
+    freehire_jobs.extend(freehire_data["data"])
 
-print("Jooble data obtained!")
-with open("data/raw/jooble_jobs.json", "w") as file:
-    json.dump(jooble_jobs, file)
-print("Jooble JSON file created")
+    params["offset"]+=params["limit"]
+
+
+print(response.status_code)
+print(freehire_data["meta"])
+print("Total jobs found: ", len(freehire_jobs))
+
+
+with open("data/raw/freehire_jobs.json", "w") as file:
+    json.dump(freehire_jobs, file)
+
+print("FreeHire JSON file created!")
