e667a093fb42da39478720e1562e9d21c54418aa
Alex Cruceru <cruceru_alex@hotmail.it>
2026-09-17 13:25:20 +0200

    Add PostgreSQL database schema

---
+++ sql/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE jobs (
+    job_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    source_job_id TEXT NOT NULL,
+    source TEXT NOT NULL,
+    title TEXT NOT NULL,
+    company TEXT,
+    location TEXT,
+    salary_min NUMERIC,
+    salary_max NUMERIC,
+    seniority TEXT,
+    work_mode TEXT,
+    published_date DATE NOT NULL,
+    UNIQUE (source, source_job_id)
+);
+
+CREATE TABLE skills(
+    skill_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    skill_name TEXT NOT NULL UNIQUE 
+);
+
+CREATE TABLE job_skills (
+    job_id INTEGER REFERENCES jobs(job_id),
+    skill_id INTEGER REFERENCES skills(skill_id),
+    PRIMARY KEY (job_id, skill_id)
+);
